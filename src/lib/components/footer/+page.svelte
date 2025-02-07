22dad11c469013b165ac1bd35511799e7f9e8c6a
SounakShome <sounakshome@gmail.com>
2025-02-07 18:20:09 +0530

    added all the component files

---
+++ src/lib/components/footer/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { page } from "$app/state"
+</script>
+
+<div>
+    <h1>Footer</h1>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
-  import { page } from "$app/state"
-  import { signOut } from "@auth/sveltekit/client";
-  import { goto } from "$app/navigation";
+  import Navbar from "$lib/components/navbar/+page.svelte";
+  import Hero from "$lib/components/hero/+page.svelte";
+  import AboutUs from "$lib/components/aboutUs/+page.svelte";
+  import Tracks from "$lib/components/tracks/+page.svelte";
+  import TimeLine from "$lib/components/timeline/+page.svelte";
+  import Prize from "$lib/components/prize_pool/+page.svelte";
+  import Sponsors from "$lib/components/sponsors/+page.svelte";
+  import Organizers from "$lib/components/organizers/+page.svelte";
+  import Faq from "$lib/components/faq/+page.svelte";
+  import Contact from "$lib/components/contactUs/+page.svelte";
+  import Footer from "$lib/components/footer/+page.svelte";
 </script>
  
-<div>
-  {#if !page.data.token}
-    <!-- landing page goes here -->
-    <button on:click={() => goto("/signIn")}>Login</button>
-  {:else}
-    <p>Hello {page.data.cookies?.test?.val}</p>
-    this is home route
-    <button on:click={() => {
-      signOut({ callbackUrl: "/signOut" });
-    }}>Log out</button>
-  {/if}
+<div class="m-10">
+  <Navbar />
+  <Hero />
+  <AboutUs />
+  <Tracks />
+  <TimeLine />
+  <Prize />
+  <Sponsors />
+  <Organizers />
+  <Faq />
+  <Contact />
+  <Footer />
 </div>
